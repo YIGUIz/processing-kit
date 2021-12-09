160b1fab230001e1c8ac4a4717b884fe30228877
YIGUIz <64197852+YIGUIz@users.noreply.github.com>
2021-12-09 11:39:54 +0800

    Update imputation_accuracy.py

--- imputation-accuracy/Code/imputation_accuracy.py
+++ imputation-accuracy/Code/imputation_accuracy.py
@@ -1,9 +1,8 @@
 #########################################################################################################################################
-# author:zq time:2021/08/20
+# author:zq time:2021/12/09
 
 # -*- coding:utf-8 -*-
 import sys,os
-import pandas as pd
 
 ###USEFUL DEFs
 def bomb(message):
@@ -16,7 +15,7 @@ def check_file(value,name):
  
 
 ###IMPORT parameter file
-PARAMFILE_OPEN=open(r'C:\Users\Administrator\Desktop\imputation-accuracy.param','r',encoding='gb18030').readlines() #Need to check
+PARAMFILE_OPEN=open(r'*\imputation-accuracy.param','r',encoding='UTF-8').readlines() #Need to check
 PARAMETERS=[ind.strip().replace("'","").replace('"','').replace(';',',') for ind in PARAMFILE_OPEN if not '#' in ind[0]]
 PARAM=[]
 for i in PARAMETERS:
@@ -24,38 +23,64 @@ for i in PARAMETERS:
     PARAM.append(i.strip().split('=')[1].strip())
 if PARAM[-1]=='\\t':PARAM[-1]='\t'
 
-if len(PARAM)!=4:bomb('Wrong number of parameters found in peddam.param. Please check it!') #Need to check
-else:ture_ped,imputated_ped,result,common =PARAM #Need to check
+if len(PARAM)!=6:bomb('Wrong number of parameters found in peddam.param. Please check it!') #Need to check
+else:true_ped,true_map,phased_ped,phased_map,result_statistic,result_snp =PARAM #Need to check
 
 ###Run check of parameters
-check_file(ture_ped,'ture_ped')
+check_file(true_ped,'true_ped')
 print("#Parameters check:OK")
 
 #########################################################################################################################################
-
-###Core DEF
-def imputation_accuracy(ture_ped,imputated_ped,result,common):
+def imputation_accuracy(true_ped,true_map,phased_ped,phased_map,result_statistic,result_snp): #phased_map = Used_snp.txt
     '''The aim of the program is calculating the accurcy of imputation.'''
     ### Read the data
-    ture_ped_dic = {}
-    common = int(common)
-    out_file = open(result,'w+')
-    out_file.write('id'+'\t'+'identical'+'\t'+'accuracy'+'\n') #cols name
-    for iline in open(ture_ped,'r'):
-        i = iline.strip().split()
-        ture_ped_dic[i[1]] = i[:]
+    phased_ped_dic = {}
+    true_snp = []
+    phased_snp = []
+    out_file1 = open(result_statistic,'w+')
+    out_file2 = open(result_snp,'w+')
+        
+    for jline in open(phased_ped,'r'):
+        j = jline.strip().split()
+        phased_ped_dic[j[1]] = j[:]
+    
+    for kline in open(true_map,'r'):
+        k = kline.strip().split()
+        true_snp.append(k[1])
+        
+    for line in open(phased_map,'r'):
+        l = line.strip().split()
+        phased_snp.append(l[1])
+        #phased_snp.append(l[0]) #notice!! the phased_map's format is unormal.
+    
+    common_snp = list(set(true_snp).intersection(phased_snp))
     
     ### The main Program
-    for jline in open(imputated_ped,'r'):
-        j = jline.strip().split()
-        ind_ped = ture_ped_dic[j[1]]
+    for mline in open(true_ped,'r'):
+        m = mline.strip().split()
+        ind = m[1]
         correct = 0
-        for k in range(6,len(ind_ped)):
-            if j[k] == ind[k]:
-                correct += 1
-        identical = correct/(len(ind_ped)-6)
-        accuracy = (correct-common)/(len(ind_ped)-6-common)
-        out_file.write(j[1]+'\t'+str(identical)+'\t'+str(accuracy)+'\n')
-    out_file.close()
-
-imputation_accuracy(ture_ped,imputated_ped,result,common)
+        miss_num = 0
+        all_common = len(common_snp)
+        ind_phased_ped = phased_ped_dic[ind] #the phased ped is benchmark
+        for n in range(6,len(ind_phased_ped),2):
+            pos = int((n-6)/2)
+            snp_name = phased_snp[pos]
+            cor_pos = true_snp.index(snp_name)
+            cor_n = cor_pos*2+6
+            if snp_name in common_snp:
+                gen1 = '%s%s'%(m[cor_n],m[cor_n+1]) #true_geno
+                gen2 = '%s%s'%(ind_phased_ped[n],ind_phased_ped[n+1]) #the step aim to distinguish AG and GA.
+                gen3 = '%s%s'%(ind_phased_ped[n+1],ind_phased_ped[n])                
+                if (gen1 == gen2) or (gen1 == gen3):
+                    correct += 1
+                else:
+                    diff = [ind,snp_name,gen1,gen2]
+                    out_file2.write('\t'.join('%s'%h for h in diff)+'\n')
+                if (gen1 == '00')or(gen2 == '00')or(gen3 == '00'):
+                    miss_num += 1
+        output = [ind,all_common,miss_num,correct]
+        out_file1.write('\t'.join('%s'%p for p in output)+'\n')
+    out_file1.close()
+    out_file2.close()
+imputation_accuracy(true_ped,true_map,phased_ped,phased_map,result_statistic,result_snp)
